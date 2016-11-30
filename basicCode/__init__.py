82e26eed739a3297f697adb3c72a80d098c3df93
yuir <lzzhikui@163.com>
2016-11-30 20:40:07 +0800

    新增加基础类型

---
+++ basicCode/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding : gb2312 -*-
+print("Hello,World!")
+print("你好")
+
+# s = input ("输入：")
+
+if True:
+    print("ture")
+else :
+        print("false")
+
+
+a,b,c = 1,1.1,"abc"
+print (a,b,c)
