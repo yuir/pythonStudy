abbd3443f7883d31095b46c9da6435f4dd417b34
yuir <lzzhikui@163.com>
2016-12-01 09:07:19 +0800

    新增加基础类型

--- basicCode/__init__.py
+++ basicCode/__init__.py
@@ -1,14 +1,6 @@
-# -*- coding : gb2312 -*-
-print("Hello,World!")
-print("你好")
+# -*- coding : utf-8 -*-
 
-# s = input ("输入：")
-
-if True:
-    print("ture")
-else :
-        print("false")
-
-
-a,b,c = 1,1.1,"abc"
-print (a,b,c)
+STRING1 = "全局变量1"
+INT1 = 1
+FLOAT1  = 1.1
+STRING2 = "初始化方法"
